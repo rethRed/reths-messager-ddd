d5b351aee7706be66217ce96968f3c72911ad7b3
rethRed <gabrielwastha@gmail.com>
2023-02-24 16:28:36 -0300

    chat message begging

---
+++ src/modules/display/display-messaging-chat/domain/entity/chat-message/chat-message.entity.ts
@@ -0,0 +1,23 @@
+import { AggregateRoot, BaseEntity } from "@/modules/@shared/domain"
+import { Either, left, right } from "@/modules/@shared/logic"
+
+
+export class ChatMessageEntity extends BaseEntity implements AggregateRoot {
+    
+    constructor(public props: ProfileEntity.Props, id?: string) {
+        super(id)
+
+
+    }
+
+}
+
+
+export namespace ProfileEntity {
+    
+
+    export type Props = {
+
+    }
+
+}
